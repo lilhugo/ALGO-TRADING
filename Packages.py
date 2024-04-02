eb42800b2c12a0b54cd87f615ad9d872faa95d8a
Hugo <hugo.fourel@yahoo.com>
2024-04-02 18:16:10 +0200

    Live PointProcess

--- Packages.py
+++ Packages.py
@@ -1,8 +1,17 @@
+import sys
+import json
+import matplotlib
+from PySide6.QtWidgets import QMainWindow, QApplication
+from PySide6.QtCore import QTimer, Qt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
+import timeit
 
+matplotlib.use('Qt5Agg')
 plt.rcParams.update({'font.size': 14})
 plt.rcParams.update({'font.family': 'serif'})
 plt.rcParams.update({'font.serif': 'Times New Roman'})
